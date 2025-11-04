6defb650bd9883a3cd179bd0b772cff8f91c084f
Clark Rowley <clarkdavidrowley@gmail.com>
2025-11-03 20:00:02 -0600

    Start intake subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -1,4 +1,31 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-public class Intake {
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
+import com.rowanmcalpin.nextftc.core.Subsystem;
+import com.rowanmcalpin.nextftc.core.command.Command;
+import com.rowanmcalpin.nextftc.core.command.utility.InstantCommand;
+import com.rowanmcalpin.nextftc.core.control.controllers.PIDFController;
+import com.rowanmcalpin.nextftc.ftc.hardware.controllables.MotorEx;
+
+public class Intake extends Subsystem {
+    private Intake() { }
+    public static final Intake INSTANCE = new Intake();
+
+    // USER CODE
+    public MotorEx motor;
+    public Command resetZero() {
+        return new InstantCommand(() -> { motor.resetEncoder(); });
+    }
+
+
+    public String name = "lift_motor";
+
+    @Override
+    public void initialize() {
+        motor = new MotorEx(name);
+
+    }
+
 }
